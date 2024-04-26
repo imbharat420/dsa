82d977361c66222b65221a7ca82e1bcfc67ac33f
imbharat420 <imbharat420@gmail.com>
2024-04-26 23:28:53 +0530

    CSLL delete

--- CSll2.py
+++ CSll2.py
@@ -59,13 +59,51 @@ class CSll:
             #     self.last = n
 
     def print_list(self):
-        temp = self.last.next
-        while temp != self.last:
-            print(temp.item)
-            temp = temp.next
-        print(temp.item)
-    # def delete_first(self):
+        if not self.is_empty():
+            temp = self.last.next
+            while temp != self.last:
+                print(temp.item,end=' ')
+                temp = temp.next
+            print(temp.item,end=' ')
+    def delete_first(self):
+        if not self.is_empty():
+            if self.last.next == self.last:
+                self.last=None
+            else:
+                self.last.next = self.last.next.next
+    def delete_last(self):
+        if not self.is_empty():
+            if self.last.next == self.last:
+                self.last=None
+            else:
+                temp=self.last.next
+                while temp !=self.last:
+                    temp=temp.next
+                temp.next=self.last.next
+                self.last=temp
+    # if empty -> agr last node ho ->   
+    def delete_after(self,data):
+        if not self.is_empty():
+            if self.last.next == self.last:
+                if self.last.item == data:
+                    self.last=None
+            else:
+                if self.last.next.item == data:
+                    self.delete_first()
+                else:
+                    temp=self.last.next
+                    
+                    while temp!=self.last:
+                        if temp.next == self.last:
+                            if self.last.item == data:
+                                self.delete_last()
+                                break
+                        if temp.next.item == data:
+                            temp.next=temp.next.next
+                            break
+                        temp=temp.next
 
+    
 
 
 cls = CSll()
