085706c7e8a439d94a76d37ff8015cf972247a6b
imbharat420 <imbharat420@gmail.com>
2024-06-05 23:19:32 +0530

    reucrsion

---
+++ Recusrion.py
@@ -0,0 +1,61 @@
+# def printN(n):
+#     if n>=1:
+#         printN(n-1)
+#         print(n,end=' ')
+# printN(10)
+
+
+# def printNreverse(n):
+#     if n>=1:
+#         print(n,end=' ')
+#         printNreverse(n-1)
+# printNreverse(10)
+
+
+# def printNodd(n):
+#     if n>=1:
+#         printNodd(n-1)
+#         print(2*n-1,end=' ')
+# printNodd(10)
+
+
+# def printNeven(n):
+#     if n>=1:
+#         printNeven(n-1)
+#         print(2*n,end=' ')
+# printNeven(10)
+
+
+# def printNevenReverse(n):
+#     if n>=1:
+#         print(2*n,end=' ')
+#         printNevenReverse(n-1)
+# printNevenReverse(10)
+
+
+
+# def sum(n):
+#     if n == 1:
+#         return 1
+#     return n+sum(n-1)
+# print(sum(3))
+
+
+# def sumOdd(n):
+#     if n == 1:
+#         return 1
+#     return 2*n-1+sumOdd(n-1)
+# print(sumOdd(5))
+
+
+# def fac(n):
+#     if n == 0 or n==1:
+#         return 1
+#     return n*fac(n-1)
+# print(fac(3))
+
+# def fac(n):
+#     if n == 0 or n==1:
+#         return 1
+#     return n*n+fac(n-1)
+# print(fac(3))
