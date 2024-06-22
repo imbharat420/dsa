46cf1ee4b074d63eeca98833c2661a6767fc1ebb
imbharat420 <imbharat420@gmail.com>
2024-06-22 15:57:23 +0530

    tree traversals

--- Tree.py
+++ Tree.py
@@ -12,9 +12,9 @@ class Tree:
         if root is None:
            return Node(data)
         elif data < root.item:
-            root.left = self.rinsert(root.left)
+            root.left = self.rinsert(root.left,data)
         elif data > root.item:
-            root.right = self.rinsert(root.left)
+            root.right = self.rinsert(root.right,data)
         return root
     def search(self,item):
         return self.rsearch(self.root,item) 
@@ -25,7 +25,7 @@ class Tree:
             return self.rsearch(self.left,item)
         else:
             return self.rsearch(self.right,item) 
-    def inorder(self):
+    def inorder(self): #Left -> Root -> Right
         result = []
         self.rinorder(self.root,result)
         return result
@@ -34,23 +34,72 @@ class Tree:
             self.rinorder(root.left,result)
             result.append(root.item)
             self.rinorder(root.right,result)
-    def preorder(self):
+    def preorder(self):   # Root -> Left -> Right
         result = []
         self.rpreorder(self.root,result)
         return result
     def rpreorder(self,root,result):
         if root:
             result.append(root.item)
-            self.rpreorder(self.left,result)
-            self.rpreorder(self.right,result)
+            self.rpreorder(root.left,result)
+            self.rpreorder(root.right,result)
 
-    def postorder(self):
+    def postorder(self): # Left -> Right -> Root
         result = []
         self.rpostorder(self.root,result)
         return result
     def rpostorder(self,root,result):
         if root:
-            self.rpostorder(self.left,result)
-            self.rpostorder(self.right,result)
+            self.rpostorder(root.left,result)
+            self.rpostorder(root.right,result)
             result.append(root.item)
-        
+
+cls = Tree()
+
+cls.insert(20)
+cls.insert(10)
+cls.insert(40)
+cls.insert(50)
+cls.insert(100)
+print("Inorder   ->",cls.inorder())
+print("Preorder  ->",cls.preorder())
+print("Postorder ->",cls.postorder())
+
+
+
+"""
+A Tree Data Structure can be traversed in following ways:
+
+Depth First Search or DFS
+Inorder Traversal
+Preorder Traversal
+Postorder Traversal
+Level Order Traversal or Breadth First Search or BFS
+
+
+Inorder Traversal:
+Inorder traversal visits the node in the order: Left -> Root -> Right
+
+
+Preorder Traversal:
+Preorder traversal visits the node in the order: Root -> Left -> Right
+
+
+Postorder Traversal: 
+Postorder traversal visits the node in the order: Left -> Right -> Root
+
+20 - 10 - 40 - 50 - 100
+
+            20
+          /   \
+         10    40
+                \
+                 50
+                  \
+                   100
+
+Inorder   -> [10, 100, 20, 10, 100]   Left -> Root -> Right
+Preorder  -> [20, 10, 100, 10, 100]   Root -> Left -> Right
+Postorder -> [100, 10, 100, 10, 20]   Left -> Right -> Root
+
+"""
